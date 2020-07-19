453409799f971508c3cf907f1660b089fbf3c8b7
Sneha Advani <sneha.d.advani@gmail.com>
2020-07-19 13:13:21 -0400

    Fixed navbar on mobile

--- src/components/SoulsmileNavbar.js
+++ src/components/SoulsmileNavbar.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../css/SoulsmileNavbar.css';
 import soulsmile from '../images/soulsmile-48.png';
+import Container from 'react-bootstrap/Container'
 
 function SoulsmileNavbar() {
   return (
