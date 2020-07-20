32a9f9c997678714a94f9bf84eee7eeb80daf7a4
Sneha Rampalli <svrampalli@gmail.com>
2020-07-20 17:11:32 -0400

    google analytics initializing app

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,9 @@ import history from '../services/history';
 import Routes from '../routes';
 import ReactGA from 'react-ga';
 
+const trackingId = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID; 
+ReactGA.initialize(trackingId);
+
 history.listen(location => {
   ReactGA.set({ page: location.pathname }); // Update the user's current page
   ReactGA.pageview(location.pathname); // Record a pageview for the given page
