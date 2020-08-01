564e563a72c4cab380644e59a72fc7166d64c056
Sneha Rampalli <svrampalli@gmail.com>
2020-08-01 17:08:03 -0400

    Added June & July 2020 Commission Report

--- src/components/MonthlyReports.js
+++ src/components/MonthlyReports.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/MonthlyReports.css';
-import June2020 from '../files/June-2020-Earnings-Report.pdf';
+import JuneJuly2020 from '../files/June-July-2020-Earnings-Report.pdf';
 
 function MonthlyReports() {
     return (
@@ -10,7 +10,7 @@ function MonthlyReports() {
         </header>  
         <hr />
         <h4 id="subtitle">Our earnings reports are posted <span className="underline">every month</span> for full transparency.</h4>
-        <a id="report" href={June2020}>June Commission Report</a>
+        <a id="report" href={JuneJuly2020}>June &amp; July 2020 Commission Report</a>
         </>
     );
 }
