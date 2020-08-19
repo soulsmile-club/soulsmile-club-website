eb3ad24182c12cf6cc8812e63e0af2859174af29
Sneha Rampalli <svrampalli@gmail.com>
2020-08-19 18:34:23 -0400

    Added nano air mask

--- src/components/Retailers.js
+++ src/components/Retailers.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import '../css/Retailers.css';
 import affiliatesData from '../files/affiliates.json';
 
-var categories = ["General", "Clothing", "Health & Beauty", "Electronics", "Home", "Food & Drink", "Shoes & Accessories", "Books", "Fitness"]
+var categories = ["General", "Clothing & Apparel", "Health & Beauty", "Electronics", "Home", "Food & Drink", "Shoes & Accessories", "Books", "Fitness"]
 
 function Retailers() {
     const [normalRetailers, setNormalRetailers] = React.useState([]);
