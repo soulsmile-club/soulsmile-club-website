a4ab0eab8f7f558a1dc820d22d8bc9b4a2261a13
Brittany Cho <brittanyalexischo@gmail.com>
2021-04-04 01:04:34 -0400

    added react hooks and airtable functionality to AffiliateBanner component

--- src/components/AffiliateBanner.js
+++ src/components/AffiliateBanner.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../css/AffiliateBanner.css';
 import ExtensionTemp from '../images/extension_temp.jpeg';
 import Button from 'react-bootstrap/Button';
 
+var REACT_APP_AIRTABLE_RETAILERS_DOC = process.env.REACT_APP_AIRTABLE_RETAILERS_DOC;
+
 function AffiliateBanner() {
-  return (
+
+    // const [normalRetailers, setNormalRetailers] = React.useState([]);
+    // const [featuredRetailers, setFeaturedRetailers] = React.useState([]);
+    const [adRetailer, setAdRetailer] = React.useState([]);
+
+    useEffect(() => {
+        fetch(REACT_APP_AIRTABLE_RETAILERS_DOC)
+			.then(res => res.json())
+			.then(res => {
+                const data = res.records;
+                data.sort(function (a, b) {
+                    return ((a["fields"]["Name"] < b["fields"]["Name"]) ? -1 : (a["fields"]["Name"] > b["fields"]["Name"]) ? 1 : 0);
+                });
+                
+                var currentAdRetailer = [];
+                for (var j = 0; j < data.length; j++) {
+                    const company = data[j]["fields"]["Name"];
+                    const link = data[j]["fields"]["Link"];
+                    // const banner = data[j]["fields"]["Banner"];
+
+                    //TEMP TEST VALUE
+                    if (company === "Girlfriend Collective") {
+                        // currentAdRetailer = [company, link, banner];
+                        currentAdRetailer = [company, link];
+                    }
+                }
+
+                setAdRetailer(currentAdRetailer);      
+			})
+			.catch(error => console.log(error));
+    }, []);
+
+
+    return (
     <>
     <style type="text/css">
-      {`
-      .btn-round {
+        {`
+        .btn-round {
         border-radius: 50px;
         margin: 10px;
         color: #444444;
-      }
-      .btn-round:hover {
+        }
+        .btn-round:hover {
         background-color: #444444;
-      }
-      `}
+        }
+        `}
     </style>
     <header className="App-header"></header>
     <div>
@@ -24,12 +59,12 @@ function AffiliateBanner() {
             <img id="affImg" alt="" src={ExtensionTemp}></img>
         </div>
         <div id="affButton">
-            <Button variant="outline-secondary btn-round" size="lg" href="http://tiny.cc/soulsmile-extension" target="_blank" rel="noopener noreferrer">Shop at affiliate with soulsmile</Button>{' '}
+            <Button variant="outline-secondary btn-round" size="lg" href={adRetailer[1]} target="_blank" rel="noopener noreferrer">Shop at {adRetailer[0]} with soulsmile</Button>{' '}
         </div>
     </div>
 
     </>
-  );
+    );
 }
 
 export default AffiliateBanner;
