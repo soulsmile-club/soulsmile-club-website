8e45b5a852e8cd25f5e26c9bb94a6757000c90df
Brittany Cho <brittanyalexischo@gmail.com>
2021-09-19 20:25:37 -0400

    updated team page

--- src/components/Team.js
+++ src/components/Team.js
@@ -7,6 +7,7 @@ import Hannah_Gonzalez from '../images/team/Hannah_Gonzalez.jpg';
 import Shuke_Zeng from '../images/team/Shuke_Zeng.jpg';
 import Regina_Oda from '../images/team/Regina_Oda.png';
 import Brittany_Cho from '../images/team/Brittany_Cho.jpg';
+import Jenny_Jiang from '../images/team/Jenny_Jiang.jpg';
 
 function Team() {
   return (
@@ -28,9 +29,38 @@ function Team() {
         to <a href="https://www.charitywater.org/" target="_blank" rel="noopener noreferrer">Charity: Water</a> and <a href="http://philosophizethis.org/" target="_blank" rel="noopener noreferrer">Philosophize This</a>.</p>
       </div>
       <div>
+        <img alt="" id="team" src={Brittany_Cho}></img>
+        <h4 id="name">Brittany Cho</h4>
+        <h5 id="role">CTO</h5>
+        <h4 id="major">Computer Science, Psychology</h4>
+        <p id="description">Hi, I’m Brittany! I believe in Soulsmile because I see an enormous potential to positively impact society through collective action when integrating social good with everyday purchases.</p>
+        <p id="description">If I could give 30 soulsmiles every month, I would give them 
+        to <a href="https://www.equalitynow.org" target="_blank" rel="noopener noreferrer">Equality Now</a> and <a href="https://www.conservation.org" target="_blank" rel="noopener noreferrer">Conservation International</a>.</p>
+      </div>
+      <div>
+        <img alt="" id="team" src={Jenny_Jiang}></img>
+        <h4 id="name">Jenny Jiang</h4>
+        <h5 id="role">CPO</h5>
+        <h4 id="major">Design Engineering, Statistics and Media</h4>
+        <p id="description">Hi I’m Jenny. As a master student in Design Engineering, I believe Soulsmile can invite more people to effortlessly participate and donate to social causes they care about.</p>
+        <p id="description">If I could give 30 soulsmiles every month, I would give them
+        to <a href="https://girlswhocode.com/" target="_blank" rel="noopener noreferrer">Girls Who Code</a> and <a href="http://tfchina.org/" target="_blank" rel="noopener noreferrer">Teach for China</a>.</p>
+      </div>
+      <div>
+        <img alt="" id="team" src={Regina_Oda}></img>
+        <h4 id="name">Regina Oda</h4>
+        <h5 id="role">CMO</h5>
+        <h4 id="major">Computer Science</h4>
+        <p id="description">Hi, I’m Regina! I believe that understanding, generosity, and goodwill can truly make a gigantic difference in our society, no matter how big or small. Helping even just one person can make the world a better place.</p>
+        <p id="description">If I could give 30 soulsmiles every month, I would give them 
+        to <a href="https://naacp.org" target="_blank" rel="noopener noreferrer">National Association for the Advancement of Colored People</a> and <a href="https://www.nokidhungry.org" target="_blank" rel="noopener noreferrer">No Kid Hungry</a>.</p>
+      </div>
+      </div>
+      <h4 id="subtitle">Our Alumni</h4>
+      <div className="flex-container-team">
+      <div>
         <img alt="" id="team" src={Sneha_Rampalli}></img>
         <h4 id="name">Sneha Rampalli</h4>
-        <h5 id="role">Co-Founder, CTO</h5>
         <h4 id="major">Computer Science</h4>
         <p id="description">Hi, I’m Sneha! As a graduate of Computer Science, I believe that 
           Soulsmile Club allows consumerism and social change to co-exist, 
@@ -41,7 +71,6 @@ function Team() {
       <div>
         <img alt="" id="team" src={Sneha_Advani}></img>
         <h4 id="name">Sneha Advani</h4>
-        <h5 id="role">Tech Developer</h5>
         <h4 id="major">Computer Science, Cognitive Science</h4>
         <p id="description">Hi, I’m Sneha! As a graduate of Computer Science and Cognitive 
         Science, I believe that human behavior is driven by empathy, kindness, 
@@ -53,7 +82,6 @@ function Team() {
       <div>
         <img alt="" id="team" src={Hannah_Gonzalez}></img>
         <h4 id="name">Hannah Gonzalez</h4>
-        <h5 id="role">Partner Outreach</h5>
         <h4 id="major">Computer Science</h4>
         <p id="description">Hi, I’m Hannah. As a sophomore studying Computer Science, 
         I envision a world where donating through your daily purchases is possible and 
@@ -64,29 +92,10 @@ function Team() {
       <div>
         <img alt="" id="team" src={Shuke_Zeng}></img>
         <h4 id="name">Shuke Zeng</h4>
-        <h5 id="role">Designer</h5>
         <h4 id="major">Computer Science, English Literature, Business</h4>
         <p id="description">Hi, I’m Shuke! I am passionate about non-profit work personally, and I believe in achieving happiness through helping others.</p>
         <p id="description">If I could give 30 soulsmiles every month, I would give them 
         to <a href="https://www.rescue.org" target="_blank" rel="noopener noreferrer">International Rescue Committee</a> and <a href="https://www.equalitynow.org" target="_blank" rel="noopener noreferrer">Equality Now</a>.</p>
-      </div>
-      <div>
-        <img alt="" id="team" src={Regina_Oda}></img>
-        <h4 id="name">Regina Oda</h4>
-        <h5 id="role">Tech Developer</h5>
-        <h4 id="major">Computer Science, English Literature, Business</h4>
-        <p id="description">Hi, I’m Regina! I believe that understanding, generosity, and goodwill can truly make a gigantic difference in our society, no matter how big or small. Helping even just one person can make the world a better place.</p>
-        <p id="description">If I could give 30 soulsmiles every month, I would give them 
-        to <a href="https://naacp.org" target="_blank" rel="noopener noreferrer">National Association for the Advancement of Colored People</a> and <a href="https://www.nokidhungry.org" target="_blank" rel="noopener noreferrer">No Kid Hungry</a>.</p>
-      </div>
-      <div>
-        <img alt="" id="team" src={Brittany_Cho}></img>
-        <h4 id="name">Brittany Cho</h4>
-        <h5 id="role">Tech Developer</h5>
-        <h4 id="major">Computer Science</h4>
-        <p id="description">Hi, I’m Brittany! I believe in Soulsmile because I see an enormous potential to positively impact society through collective action when integrating social good with everyday purchases.</p>
-        <p id="description">If I could give 30 soulsmiles every month, I would give them 
-        to <a href="https://www.equalitynow.org" target="_blank" rel="noopener noreferrer">Equality Now</a> and <a href="https://www.conservation.org" target="_blank" rel="noopener noreferrer">Conservation International</a>.</p>
       </div>
     </div>
     </>
